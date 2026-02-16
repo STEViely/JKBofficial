fe70fe126e6ac043dfaf73e570a479897a7107bd
BenSTV <stevielyfiles@gmail.com>
2026-02-17 03:27:41 +0700

    fix vercel

---
+++ api/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const serverless = require("serverless-http");
+
+const app = express();
+
+app.get("/api/photos", (req, res) => {
+  res.json({ message: "Photos API working 🚀" });
+});
+
+module.exports = app;
+module.exports.handler = serverless(app);
