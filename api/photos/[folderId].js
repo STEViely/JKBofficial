53c526640f153edd4598ab5ab8c5c4d2caec32e5
unknown <stevielyfiles@gmail.com>
2026-02-18 13:37:44 +0700

    force node runtime

--- api/photos/[folderId].js
+++ api/photos/[folderId].js
@@ -1,110 +1,8 @@
-// import { google } from "googleapis";
-
-// export default async function handler(req, res) {
-//   try {
-//     const { folderId, fileId, download } = req.query;
-
-//     const auth = new google.auth.GoogleAuth({
-//       credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON),
-//       scopes: ["https://www.googleapis.com/auth/drive.readonly"],
-//     });
-
-//     const drive = google.drive({ version: "v3", auth });
-
-//     /* ================= DOWNLOAD FILE ================= */
-
-//     if (download && fileId) {
-//       const fileMeta = await drive.files.get({
-//         fileId,
-//         fields: "name, mimeType",
-//       });
-
-//       const fileName = fileMeta.data.name;
-//       const mimeType = fileMeta.data.mimeType;
-
-//       const fileStream = await drive.files.get(
-//         {
-//           fileId,
-//           alt: "media",
-//         },
-//         { responseType: "stream" },
-//       );
-
-//       res.setHeader(
-//         "Content-Disposition",
-//         `attachment; filename="${fileName}"`,
-//       );
-//       res.setHeader("Content-Type", mimeType);
-
-//       fileStream.data.pipe(res);
-//       return;
-//     }
-
-//     /* ================= FETCH FOLDER ================= */
-
-//     if (!folderId) {
-//       return res.status(400).json({ error: "Missing folderId" });
-//     }
-
-//     const folderMeta = await drive.files.get({
-//       fileId: folderId,
-//       fields: "name",
-//     });
-
-//     const folderName = folderMeta.data.name || "Event Gallery";
-
-//     const response = await drive.files.list({
-//       q: `'${folderId}' in parents and trashed = false`,
-//       fields: "files(id, name, mimeType, createdTime)",
-//       orderBy: "createdTime desc",
-//     });
-
-//     const files = (response.data.files || []).map((file) => {
-//       const isFolder = file.mimeType === "application/vnd.google-apps.folder";
-
-//       return {
-//         id: file.id,
-//         name: file.name,
-//         type: isFolder ? "folder" : "image",
-//         createdTime: file.createdTime || null,
-
-//         previewUrl: !isFolder
-//           ? `https://drive.google.com/thumbnail?id=${file.id}&sz=w1000`
-//           : null,
-
-//         // 👇 เปลี่ยนมาใช้ API ตัวเอง
-//         downloadUrl: !isFolder
-//           ? `/api/photos/${folderId}?download=1&fileId=${file.id}`
-//           : null,
-//       };
-//     });
-
-//     return res.status(200).json({
-//       folderName,
-//       files,
-//     });
-//   } catch (error) {
-//     console.error("Drive API error:", error);
-//     return res.status(500).json({
-//       error: "Failed to fetch data",
-//       details: error.message,
-//     });
-//   }
-// }
-
-export const config = {
-  runtime: "nodejs",
-};
-
 import { google } from "googleapis";
 
 export default async function handler(req, res) {
   try {
-    const { folderId } = req.query;
-
-    if (!folderId) {
-      return res.status(400).json({ error: "Missing folderId" });
-    }
+    const { folderId, fileId, download } = req.query;
 
     const auth = new google.auth.GoogleAuth({
       credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON),
@@ -113,36 +11,76 @@ export default async function handler(req, res) {
 
     const drive = google.drive({ version: "v3", auth });
 
-    let allFiles = [];
-    let nextPageToken = null;
+    /* ================= DOWNLOAD FILE ================= */
 
-    do {
-      const response = await drive.files.list({
-        q: `'${folderId}' in parents and trashed = false`,
-        fields: "nextPageToken, files(id, name, mimeType, createdTime)",
-        orderBy: "createdTime desc",
-        pageSize: 1000,
-        pageToken: nextPageToken || undefined,
+    if (download && fileId) {
+      const fileMeta = await drive.files.get({
+        fileId,
+        fields: "name, mimeType",
       });
 
-      allFiles = [...allFiles, ...(response.data.files || [])];
-      nextPageToken = response.data.nextPageToken;
-    } while (nextPageToken);
+      const fileName = fileMeta.data.name;
+      const mimeType = fileMeta.data.mimeType;
 
-    const files = allFiles
-      .filter(
-        (file) => file.mimeType !== "application/vnd.google-apps.folder"
-      )
-      .map((file) => ({
+      const fileStream = await drive.files.get(
+        {
+          fileId,
+          alt: "media",
+        },
+        { responseType: "stream" },
+      );
+
+      res.setHeader(
+        "Content-Disposition",
+        `attachment; filename="${fileName}"`,
+      );
+      res.setHeader("Content-Type", mimeType);
+
+      fileStream.data.pipe(res);
+      return;
+    }
+
+    /* ================= FETCH FOLDER ================= */
+
+    if (!folderId) {
+      return res.status(400).json({ error: "Missing folderId" });
+    }
+
+    const folderMeta = await drive.files.get({
+      fileId: folderId,
+      fields: "name",
+    });
+
+    const folderName = folderMeta.data.name || "Event Gallery";
+
+    const response = await drive.files.list({
+      q: `'${folderId}' in parents and trashed = false`,
+      fields: "files(id, name, mimeType, createdTime)",
+      orderBy: "createdTime desc",
+    });
+
+    const files = (response.data.files || []).map((file) => {
+      const isFolder = file.mimeType === "application/vnd.google-apps.folder";
+
+      return {
         id: file.id,
         name: file.name,
+        type: isFolder ? "folder" : "image",
         createdTime: file.createdTime || null,
-        previewUrl: `https://drive.google.com/thumbnail?id=${file.id}&sz=w1000`,
-        downloadUrl: `/api/photos/${folderId}?download=1&fileId=${file.id}`,
-      }));
+
+        previewUrl: !isFolder
+          ? `https://drive.google.com/thumbnail?id=${file.id}&sz=w1000`
+          : null,
+
+        // 👇 เปลี่ยนมาใช้ API ตัวเอง
+        downloadUrl: !isFolder
+          ? `/api/photos/${folderId}?download=1&fileId=${file.id}`
+          : null,
+      };
+    });
 
     return res.status(200).json({
-      folderName: "Event Gallery",
+      folderName,
       files,
     });
   } catch (error) {
@@ -153,3 +91,4 @@ export default async function handler(req, res) {
     });
   }
 }
+
